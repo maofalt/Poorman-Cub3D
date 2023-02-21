45ae6bcf4fda9372100cb653ce6e64cd78619527
motero <motero@student.42.fr>
2023-02-21 13:14:07 +0100

    main

--- includes/structures.h
+++ includes/structures.h
@@ -10,6 +10,20 @@
 /*                              STRUCTURES                                    */
 /*############################################################################*/
 
+/* Vector structure for 2D float vector
+** Vector structure for 2D unsigned int vector
+** Vector structure for 2D int vectors
+** causes the compiler to set the mode for foo, 
+** to be 16 bytes, divided into int sized units.
+** SIMD vectorrization 
+** https://users.ece.cmu.edu/~franzf/teaching/slides-18-645-simd.pdf
+** http://gcc.gnu.org/onlinedocs/gcc-4.6.1/gcc/Vector-Extensions.html#Vector-Extensions
+*/
+
+typedef float			t_vector_f __attribute__((vector_size (8)));
+typedef unsigned int	t_vector_u __attribute__((vector_size (8)));
+typedef int				t_vector_i __attribute__((vector_size (8)));
+
 /* Parsing structure for CUB3D stocking information from .cub file
 ** a wolrd map in a 2D int array of MAPWIDTH * MAPHEIGHT
 ** a array  of size 4 of sprites using t_image from the mlx
@@ -25,24 +39,7 @@ typdef struct s_cub
 	size_t		mapwidth;
 	size_t		mapheight;
 	int			**map;
-	t_image		sprite[4];
-	
+	t_img		texture[4];
 }				t_cub;
-
-
-/* Vector structure for 2D float vector
-** Vector structure for 2D unsigned int vector
-** Vector structure for 2D int vectors
-** causes the compiler to set the mode for foo, 
-** to be 16 bytes, divided into int sized units.
-** SIMD vectorrization 
-** https://users.ece.cmu.edu/~franzf/teaching/slides-18-645-simd.pdf
-** http://gcc.gnu.org/onlinedocs/gcc-4.6.1/gcc/Vector-Extensions.html#Vector-Extensions
-
-*/
-
-typedef float			t_vector_f __attribute__((vector_size (8)));
-typedef unsigned int	t_vector_u __attribute__((vector_size (8)));
-typedef int				t_vector_i __attribute__((vector_size (8)));
 
 #endif
