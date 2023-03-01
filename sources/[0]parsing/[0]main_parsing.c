4b416b5636034b84c3bb528e6861937a2ea81599
yanthoma <yanthoma@student.42.fr>
2023-03-02 00:04:37 +0100

    norminette

--- sources/[0]parsing/[0]main_parsing.c
+++ sources/[0]parsing/[0]main_parsing.c
@@ -6,7 +6,7 @@
 /*   By: yanthoma <yanthoma@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/02/21 11:30:35 by motero            #+#    #+#             */
-/*   Updated: 2023/03/01 23:50:56 by yanthoma         ###   ########.fr       */
+/*   Updated: 2023/03/01 23:59:12 by yanthoma         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,7 +19,7 @@ int	main_parsing(t_cub	*data, char *path)
 	if (!parsing_text(data, path))
 		return (1);
 	if (!check_wall(data->map))
-	 	return(printf("nein\n"), 1);
+		return (printf("nein\n"), 1);
 	if (!get_player_pos(data->map))
 		return (printf("more tha one or none nigga\n"), 1);
 	free_double_char(data->map);
